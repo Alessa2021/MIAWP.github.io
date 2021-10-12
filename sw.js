99e9942eb012ba4427fe6463e9737be89e8cd105
Alessa2021 <z.prende.a@gmail.com>
2021-10-11 21:33:34 -0300

    MIAWP

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', e =>{
+    console.log('sw Install')
+})
+
+self.addEventListener('activate', e =>{
+    console.log('sw Activate')
+})
+
+self.addEventListener('fetch', e =>{
+    console.log('sw Fetch')
+})
